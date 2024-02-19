69de5c22bb30c6c7a942cef64f072d0d6bff9da0
Satyam <satyamt@mindfiresolutions.com>
2024-02-19 18:25:37 +0530

    change package path

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
             targets: ["MobileQuickLaunchKit"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/Satyam-Mindfire/Package-All", from: "1.0.0"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "9.0.0"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0"),
         // Add other dependencies as needed
@@ -24,6 +25,8 @@ let package = Package(
         .target(
             name: "MobileQuickLaunchKit",
             dependencies: [
+                .product(name: "MQLCoreUI", package: "package-all"),
+                .product(name: "MQLCore", package: "package-all"),
                 .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
                 .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
