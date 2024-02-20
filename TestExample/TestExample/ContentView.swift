51b9030d002d62d6d3827fb368b3545422fcd3d7
Satyam <satyamt@mindfiresolutions.com>
2024-02-20 10:52:55 +0530

    Add example project

---
+++ TestExample/TestExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestExample
+//
+//  Created by Satyam Tripathi on 20/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestExample/TestExample/TestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestExampleApp.swift
+//  TestExample
+//
+//  Created by Satyam Tripathi on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
